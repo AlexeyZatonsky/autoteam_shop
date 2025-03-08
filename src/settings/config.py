0a285474aa0f8ca2fd353995070c2b804dd42ba6
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-03-08 14:24:08 +0300

    первые настройки репа

---
+++ src/settings/config.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    
+    DB_USER: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_PASS: str
+    SECRET_AUTH: str
+    SERVER_HOST: str
+    SERVER_PORT: int
+    
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
